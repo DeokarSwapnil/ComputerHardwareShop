0c9d505975df5961480dbd78e93c6afe21eef156
Swapnil Deokar <54211718+DeokarSwapnil@users.noreply.github.com>
2021-08-30 22:28:35 +0530

    Add files via upload

---
+++ StatusClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shop.db;
+
+/**
+ *
+ * @author star
+ */
+public class StatusClass {
+    public static String userid;
+    public static String user;
+    public static String pass;
+    
+}
